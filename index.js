588385bbff79afeb9b2edbbf0cef487c41b98ac4
Luis <pereira1214@gmail.com>
2021-08-08 13:39:45 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+    return string.toUpperCase("Hello!");
+}
+function whisper(string) {
+    return string.toLowerCase("HELLO!");
+}
+function logShout(string) {
+    console.log(string.toUpperCase("Hello!)"))
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase("Hello!)"))
+}
+function sayHiToGrandma(string) {
+    if (string.toLowerCase("") === string) {
+        return ("I can't hear you!")
+    } else if (string.toUpperCase("") === string) {
+        return ("YES INDEED!")
+    } else if (string === "I love you, Grandma.") {
+        return ("I love you, too.")
+    } 
+}
+
